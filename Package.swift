a297ba017fee318e66930e625a791326e272405c
Ivan Abramov <42469397+TopIvanAbramov@users.noreply.github.com>
2025-12-09 23:58:36 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,17 +1,22 @@
-// swift-tools-version: 5.7.0
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
     name: "MadSDK",
-    platforms: [ .iOS(.v15) ],
+    platforms: [.iOS(.v15)],
     products: [
-        .library(name: "MadSDK", targets: ["MadSDK"])
+        .library(name: "MadSDK", targets: ["MadSDK", "MadsCore"])
     ],
     targets: [
         .binaryTarget(
             name: "MadSDK",
-            url: "https://github.com/TopIvanAbramov/MadSDK-ios/releases/download/0.0.3/MadSDK.xcframework.zip",
+            url: "https://github.com/TopIvanAbramov/MadSDK-ios/releases/download/0.0.4/MadSDK.xcframework.zip",
             checksum: "40c601703a2bb81ea78e824ead169a46cffae1af8e07d37df2ce4b66d9996a01"
+        ),
+        .binaryTarget(
+            name: "MadsCore",
+            url: "https://github.com/TopIvanAbramov/MadSDK-ios/releases/download/0.0.4/MadsCore.xcframework.zip",
+            checksum: "ce0238a89aeb5ea90d235ba5a968d67c8d2486ba0b79464a8be1dace97e10ffc"
         )
     ]
 )
