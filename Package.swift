a703d5b219077c623c08c3e0489773d60ed19e3e
Ivan Abramov <42469397+TopIvanAbramov@users.noreply.github.com>
2025-12-10 18:23:02 +0300

    Refactor Package.swift to simplify targets
    Removed MadsCore binary target and updated MadSDK target path.

--- Package.swift
+++ Package.swift
@@ -3,20 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "MadSDK",
-    platforms: [.iOS(.v15)],
+    platforms: [ .iOS(.v15) ],
     products: [
-        .library(name: "MadSDK", targets: ["MadSDK", "MadsCore"])
+        .library(name: "MadSDK", targets: ["MadSDK"])
     ],
     targets: [
         .binaryTarget(
             name: "MadSDK",
-            url: "https://github.com/TopIvanAbramov/MadSDK-ios/releases/download/0.0.4/MadSDK.xcframework.zip",
-            checksum: "40c601703a2bb81ea78e824ead169a46cffae1af8e07d37df2ce4b66d9996a01"
-        ),
-        .binaryTarget(
-            name: "MadsCore",
-            url: "https://github.com/TopIvanAbramov/MadSDK-ios/releases/download/0.0.4/MadsCore.xcframework.zip",
-            checksum: "ce0238a89aeb5ea90d235ba5a968d67c8d2486ba0b79464a8be1dace97e10ffc"
+            path: "MadSDK.xcframework"
         )
     ]
 )
