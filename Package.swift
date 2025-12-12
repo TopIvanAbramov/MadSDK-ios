fcb48dd8aec75f71ad1367e267e9a7c6c7467ba6
abramov_ia <ivan_abramov@itms.tech>
2025-12-12 14:28:32 +0300

    Try to fix framework and package again

--- Package.swift
+++ Package.swift
@@ -5,12 +5,23 @@ let package = Package(
     name: "MadSDK",
     platforms: [ .iOS(.v15) ],
     products: [
-        .library(name: "MadSDK", targets: ["MadSDKBinary"])
+        .library(name: "MadSDK", targets: ["MadSDK"])
     ],
     targets: [
+        .target(
+            name: "MadSDK",
+            dependencies: ["MadSDKBinary", "MadsCoreBinary"],
+            linkerSettings: [
+                .linkedFramework("MadsCore")
+            ]
+        ),
         .binaryTarget(
             name: "MadSDKBinary",
             path: "MadSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "MadsCoreBinary",
+            path: "MadsCore.xcframework"
         )
     ]
 )
